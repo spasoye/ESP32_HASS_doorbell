443204795418ed2e65b27cd2f7d37ecd0fb4ed11
Ivan <ivan@spasoye.com>
2025-08-31 17:43:18 +0200

    Implement ry-except-finally block into connect functions.

--- connect.py
+++ connect.py
@@ -1,14 +1,24 @@
 import network
 import time
 import config
+import machine
 
 def connect_wifi():
     """Connects to Wi-Fi and returns the network interface."""
+    print("Connecting to "+config.SSID) 
     wlan = network.WLAN(network.STA_IF)
+    time.sleep(1)
     wlan.active(True)
-    wlan.connect(config.SSID, config.PASSWORD)
-    while not wlan.isconnected():
+    time.sleep(1)
+    try:
+        wlan.connect(config.SSID, config.PASSWORD)
         time.sleep(1)
-    print(f'Connected! IP: {wlan.ifconfig()[0]}. Open this IP in your browser')
-    return wlan
+        while not wlan.isconnected():
+            machine.idle()
+    except:
+        print("Error while tryin to connect")
+        return "ERROR"
+    finally:
+        print(f'Connected! IP: {wlan.ifconfig()[0]}. Open this IP in your browser')
+        return wlan
 
